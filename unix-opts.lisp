55c235c8a5aa97bd758ab5045feca68e4001f06f
mrkkrp <markkarpov@opmbx.org>
2015-02-23 23:40:47 +0600

    added some stuff

--- unix-opts.lisp
+++ unix-opts.lisp
@@ -26,6 +26,51 @@
 (defpackage   :unix-opts
   (:nicknames :opts)
   (:use       #:common-lisp)
-  (:export    ))
+  (:export    #:define
+              #:argv
+              #:get
+              #:describe)
+  (:shadow    #:get
+              #:describe))
 
 (in-package #:unix-opts)
+
+(defmacro define (&rest descriptions)
+  "Define command line options. DESCRIPTIONS must be a list, where every
+element is also a list. CAR of this list must be a keyword denoting name of
+the option. CADR of this list must be a string describing the option. Next,
+there may be a character (short option) and/or string (long option). If
+particular option should take an argument, you may add a function that will
+be called to parse the argument in form of string into your desired
+type. This library provides basic parsers, but you can write and use your
+own."
+  (declare (ignore descriptions)))
+
+(defun argv ()
+  "Return list of program's arguments, including command used to execute the
+program as first elements of the list."
+  #+abcl      ext:*command-line-argument-list*
+  #+allegro   sys:command-line-arguments
+  #+:ccl      ccl:*command-line-argument-list*
+  #+clisp     ext:argv
+  #+clozure   ccl::command-line-arguments
+  #+cmu       extensions:*command-line-words*
+  #+ecl       (ext:command-args)
+  #+gcl       si:*command-args*
+  #+lispworks system:*line-arguments-list*
+  #+sbcl      sb-ext:*posix-argv*)
+
+(defun get (&optional options)
+  "Parse command line options. If OPTIONS is given, it should be a list to
+parse. If it's not given, the function will use ARGV function to get list of
+command line arguments. Return two values: list that contains keywords
+associated with command line options with DEFINE-OPTS macro, and list of
+free arguments. If some option requires an argument, you can use GETF to
+test presence of the option and get its argument if the option is present."
+  (declare (ignore options)))
+
+(defun describe (&key alpha-sort)
+  "Return string describing options of the program that were defined with
+DEFINE macro previously. If argument ALPHA-SORT is given and it's not NIL,
+descriptions of the options will be sorted alphabetically."
+  (declare (ignore alpha-sort)))
