537e5b6176096e8e8cdb6c1bd62417539b6fa042
Thomas Schaper <thomas@libremail.nl>
2020-05-24 18:08:51 +0200

    Try to fix coveralls

--- run-tests.bash
+++ run-tests.bash
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 if [[ -n "$COVERALLS" ]]; then
-    cl -l unix-opts -l unix-opts/tests -l cl-coveralls \
-        -e '(setf *debugger-hook*
-                 (lambda (c h)
-                   (declare (ignore c h))
-                   (uiop:quit 1)))' \
-        -e '(coveralls:with-coveralls (:exclude "./unix-opts-test.lisp")
-             (unix-opts/tests:run))'
+    cl -l unix-opts/tests -e '(values)'
+    cl -l unix-opts/tests -l cl-coveralls \
+        -e '(progn
+              (ql:quickload :cl-coveralls)
+              (coveralls:with-coveralls ()
+                (unix-opts/tests:run)))'
 
 else
-    cl -l unix-opts -l unix-opts/tests \
+    cl -l unix-opts/tests \
         -e '(progn
               (uiop:quit (if (unix-opts/tests:run) 0 1)))'
 
