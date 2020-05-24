7f22d4eb148a6f07c2f833fc733d24553f7e89f1
Thomas Schaper <thomas@libremail.nl>
2020-05-24 19:11:56 +0200

    Revert "Move to roswell"
    This reverts commit 6bc6066b4381baa4b04d7cf54e053ce69cafb46b.

--- run-tests.bash
+++ run-tests.bash
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 if [[ -n "$COVERALLS" ]]; then
-    ros \
+    cl -l unix-opts/tests -e '(values)'
+    cl -l unix-opts/tests -l cl-coveralls \
         -e '(progn
               (ql:quickload :cl-coveralls)
-              (ql:quickload :unix-opts/tests)
-              (let (suc)
-                (coveralls:with-coveralls ()
-                  (setf suc (unix-opts/tests:run))))
-              (uiop:quit (if suc 0 1)))'
+              (coveralls:with-coveralls ()
+                (unix-opts/tests:run)))'
 
 else
-    ros \
+    cl -l unix-opts/tests \
         -e '(progn
-              (ql:quickload :unix-opts/tests)
               (uiop:quit (if (unix-opts/tests:run) 0 1)))'
 
 fi
