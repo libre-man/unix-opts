ae65e7ff0e35649526717129cd62cdeadff5dc9f
Thomas Schaper <thomas@libremail.nl>
2020-05-24 19:12:10 +0200

    Actually use cl-coveralls not coveralls

--- run-tests.bash
+++ run-tests.bash
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 if [[ -n "$COVERALLS" ]]; then
-    cl -l unix-opts/tests -e '(values)'
-    cl -l unix-opts/tests -l cl-coveralls \
-        -e '(progn
-              (ql:quickload :cl-coveralls)
-              (coveralls:with-coveralls ()
-                (unix-opts/tests:run)))'
+    ros -e '(ql:quickload :cl-coveralls)
+            (cl-coveralls.impls:enable-coverage)
+            (asdf:load-system :unix-opts/tests)
+            (asdf:load-system :unix-opts)
+            (let (suc)
+              (cl-coveralls:with-coveralls (:exclude (list "tests.lisp"))
+                (setf suc (unix-opts/tests:run)))
+              (unix-opts:exit (if suc 0 1)))'
 
 else
-    cl -l unix-opts/tests \
-        -e '(progn
-              (uiop:quit (if (unix-opts/tests:run) 0 1)))'
+    cl -l unix-opts/tests -l unix-opts \
+        -e '(unix-opts:exit (if (unix-opts/tests:run) 0 1))'
 
 fi
