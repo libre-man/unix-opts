d5d8ed34a5ba7189fe43db8c3ed0bd4f4bf5258b
Thomas Fitzsimmons <fitzsim@fitzsim.org>
2022-01-20 02:19:53 -0500

    Do not return ECL --shell and script arguments
    When invoked with sbcl --script file.lisp ..., sb-ext:*posix-argv* does
    not contain the arguments --script and file.lisp.  The equivalent for
    ecl --shell file.lisp ... is the variable
    system:*unprocessed-ecl-command-args*.  The function ext:command-args
    included --shell and file.lisp, causing unix-opts under that ECL
    invocation to produce an error, 'unknown option: "--shell".'

--- unix-opts.lisp
+++ unix-opts.lisp
@@ -244,7 +244,7 @@ the program as first elements of the list. Portable across implementations."
   #+clisp     (cons *load-truename* ext:*args*)
   #+clozure   ccl:*command-line-argument-list*
   #+cmu       extensions:*command-line-words*
-  #+ecl       (ext:command-args)
+  #+ecl       system:*unprocessed-ecl-command-args*
   #+gcl       si:*command-args*
   #+lispworks system:*line-arguments-list*
   #+sbcl      sb-ext:*posix-argv*)
